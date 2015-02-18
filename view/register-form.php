02534b9da4cc2208b3f05c84dac4a1a6502d08fd
sean escobedo <seancandoit68@gmail.com>
2015-02-18 09:20:03 -0800

    young blood

--- view/register-form.php
+++ view/register-form.php
@@ -1,19 +1,28 @@
-<form>
+<?php
+    require_once(__DIR__ . "/../model/config.php"); 
+?>
+
+<h1>Register</h1>
+
+<form method="post" action="<?php echo $path . "controller/create-user.php";?>">
     <div>
         <label for="email">Email: </label>
         <input type="text" name=" email" /> 
     </div>
-    
+    <br />
+    <br />
     <div>
         <label for="username">Username: </label>
         <input type="text" name="username" />
     </div>
-    
+    <br />
+    <br />
     <div>
         <label for="password">Password: </label>
         <input type="password" name="password" />
     </div>
-    
+    <br />
+    <br />
     <div>
         <button type="submit">Enter</button>
     </div>
