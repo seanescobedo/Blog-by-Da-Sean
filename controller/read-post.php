c77d0ebb6af4be9488976ae9ddab3f9035d876aa
sean escobedo <seancandoit68@gmail.com>
2015-02-23 10:44:47 -0800

    omfg

--- controller/read-post.php
+++ controller/read-post.php
@@ -2,5 +2,16 @@
 
    require_once(__DIR__ . "/../model/config.php");
    
-   $query = "SELECT * FROM post";
-   $result = 
+   $query = "SELECT * FROM posts";
+   $result = $_SESSION["connection"]->query($query);
+   
+   if($result){
+       while($row = mysqli_fetch_array($result)){
+           echo "<div class='post'>";
+           echo "<h2>" . $row['title'] . "</h2>";
+           echo "<br />";
+           echo "<p>" . $row['post'] . "</h2>";
+           echo "<br />";
+           echo "</div>";
+       }
+   }
--- index.php
+++ index.php
@@ -4,6 +4,6 @@
     require_once(__DIR__ . "/view/navigation.php");
     require_once(__DIR__ . "/controller/create-db.php");
     require_once(__DIR__ . "/view/footer.php");
-    require_once(__DIR__ . "/controller /read-post.php");
+    require_once(__DIR__ . "/controller/read-post.php");
  ?>
    
